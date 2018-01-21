3dce605199aabb80a6b227c42360e3be8af452a3
klinga <klinga@users.noreply.github.com>
2018-01-21 17:06:23 -0500

    data optimized

--- DeweyVis/import2datastore.py
+++ DeweyVis/import2datastore.py
@@ -2,7 +2,9 @@
 
 import unicodecsv as csv
 
-from datastore import Bib, Item, session_scope, insert_or_ignore
+from datastore import (Bib, Item, session_scope, insert_or_ignore,
+                       Bstatus, Country, Lang, CatSource, CatAgency,
+                       Istatus, Location)
 
 
 fh = './files/bpl_171220_raw.csv'
@@ -10,30 +12,57 @@ with open(fh, 'r') as csvfile:
     reader = csv.reader(csvfile, encoding='utf-8')
     print list(enumerate(reader.next()))
 
-    for row in reader:
-    # for x in range(5):
-        # row = reader.next()
+    # for row in reader:
+    for x in range(5):
+        row = reader.next()
         with session_scope() as session:
+            bstatus = dict(
+                code=row[3])
+            insert_or_ignore(session, Bstatus, **bstatus)
+            country = dict(
+                code=row[6])
+            insert_or_ignore(session, Country, **country)
+            lang = dict(
+                code=row[8])
+            insert_or_ignore(session, Lang, **lang)
+            catSrc = dict(
+                code=row[9])
+            insert_or_ignore(session, CatSource, **catSrc)
+            catAgn = dict(
+                code=row[10])
+            insert_or_ignore(session, CatAgency, **catAgn)
+
+            session.flush()
+
             bib = dict(
                 bid=row[0],
                 dateCreated=row[1],
                 dateCataloged=row[2],
-                status=row[3],
+                bsid=bstatus.bsid,
                 itemCount=row[4],
                 callNo=row[5],
-                country=row[6],
+                cid=country.cid,
                 pubDate=row[7],
-                lang=row[8],
-                catSource=row[9],
-                catAgency=row[10])
+                laid=lang.laid,
+                csid=catSrc.casid,
+                caid=catAgn.caid)
             insert_or_ignore(session, Bib, **bib)
+
+            iStatus = dict(
+                code=row[13])
+            insert_or_ignore(session, Istatus, **iStatus)
+            loc = dict(
+                code=row[14])
+            insert_or_ignore(session, Location, **loc)
+
+            session.flush()
 
             item = dict(
                 iid=row[11],
                 bid=row[0],
                 dateCreated=row[12],
-                status=row[13],
-                location=row[14],
+                isid=iStatus.isid,
+                loid=loc.loid,
                 checkout=row[15],
                 renewal=row[16],
                 lastCheckout=row[17])
