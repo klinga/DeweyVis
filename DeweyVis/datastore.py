f1854053133142fbb36242fe19e8b65854e1affa
klinga <klinga@users.noreply.github.com>
2018-01-21 12:45:09 -0500

    optimized tables

--- DeweyVis/datastore.py
+++ DeweyVis/datastore.py
@@ -14,14 +14,14 @@ class Bib(Base):
     bid = Column(Integer, primary_key=True, autoincrement=False)
     dateCreated = Column(String)
     dateCataloged = Column(String)
-    status = Column(String)
+    bsid = Column(Integer, ForeignKey('bStatus.bsid'))
     itemCount = Column(Integer)
     callNo = Column(String)
-    country = Column(String)
+    cid = Column(Integer, ForeignKey('country.cid'))
     pubDate = Column(String)
-    lang = Column(String)
-    catSource = Column(String)
-    catAgency = Column(String)
+    laid = Column(Integer, ForeignKey('lang.laid'))
+    csid = Column(Integer, ForeignKey('catSource.csid'))
+    caid = Column(Integer, ForeignKey('catAgency.caid'))
 
     items = relationship('Item', cascade='all, delete-orphan')
 
@@ -34,13 +34,63 @@ class Bib(Base):
                 self.lang, self.catSource, self.catAgency)
 
 
+class Bstatus(Base):
+    __tablename__ = 'bStatus'
+    bsid = Column(Integer, primary_key=True)
+    code = Column(String)
+
+    def __repr__(self):
+        return "<Bstatus(bsid='%s', bid='%s', code='%s')>" % (
+            self.bsid, self.bid, self.code)
+
+
+class Country(Base):
+    __tablename__ = 'country'
+    cid = Column(Integer, primary_key=True)
+    code = Column(String)
+
+    def __repr__(self):
+        return "<Country(cid='%s', bid='%s', code='%s')>" % (
+            self.cid, self.bid, self.code)
+
+
+class Lang(Base):
+    __tablename__ = 'lang'
+    laid = Column(Integer, primary_key=True)
+    code = Column(String)
+
+    def __repr__(self):
+        return "<Lang(lid='%s', code='%s')>" % (
+            self.lid, self.code)
+
+
+class CatSource(Base):
+    __tablename__ = 'catSource'
+    csid = Column(Integer, primary_key=True)
+    code = Column(String)
+
+    def __repr__(self):
+        return "<CatSource(csid='%s', code='%s')>" % (
+            self.csid, self.code)
+
+
+class CatAgency(Base):
+    __tablename__ = 'catAgency'
+    caid = Column(Integer, primary_key=True)
+    code = Column(String)
+
+    def __repr__(self):
+        return "<CatAgency(caid='%s', code='%s')>" % (
+            self.caid, self.code)
+
+
 class Item(Base):
     __tablename__ = 'item'
     iid = Column(Integer, primary_key=True, autoincrement=False)
     bid = Column(Integer, ForeignKey('bib.bid'), nullable=False)
     dateCreated = Column(String)
-    status = Column(String)
-    location = Column(String)
+    isid = Column(Integer, ForeignKey('iStatus.isid'))
+    location = Column(Integer, ForeignKey('location.loid'))
     checkout = Column(Integer)
     renewal = Column(Integer)
     lastCheckout = Column(String)
@@ -52,6 +102,26 @@ class Item(Base):
                 self.iid, self.dateCreated, self.status,
                 self.location, self.checkout, self.renewal,
                 self.lastCheckout)
+
+
+class Istatus(Base):
+    __tablename__ = 'iStatus'
+    isid = Column(Integer, primary_key=True)
+    code = Column(String)
+
+    def __repr__(self):
+        return "<Istatus(isid='%s', code='%s')>" % (
+            self.isid, self.code)
+
+
+class Location(Base):
+    __tablename__ = 'location'
+    loid = Column(Integer, primary_key=True)
+    code = Column(String)
+
+    def __repr__(self):
+        return "<Location(loid='%s', code='%s')>" % (
+            self.loid, self.code)
 
 
 class DataAccessLayer:
@@ -71,7 +141,6 @@ dal = DataAccessLayer()
 
 @contextmanager
 def session_scope():
-    dal.connect()
     session = dal.Session()
     try:
         yield session
@@ -82,12 +151,20 @@ def session_scope():
         session.close()
 
 
-def insert_or_ignore(session, model, **kwargs):
+def insert_or_ignore_orm(session, model, **kwargs):
     instance = session.query(model).filter_by(**kwargs).first()
     if not instance:
         instance = model(**kwargs)
         session.add(instance)
 
+
+def insert_or_ignore_core(session, ins):
+
+
+
+
+
 if __name__ == '__main__':
+    # create datastore schema before inserting data
     dal.connect()
 
